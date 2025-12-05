537cef760930aee11e9570e0ff3f0313fe010d4a
jb12-art <jbqw1@msn.com>
2025-12-05 14:49:36 +0200

    files

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-describe("Greet Function", () => {
-  it("should return the name provided", () => {
-    const result = greet("John");
-    expect(result).toBe("John");
-  });
-});
 
-function greet(name) {
-  return name;
-}
